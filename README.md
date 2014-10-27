f538e71363fd3ca026b3c3320d5ea6bf5f11bb47
Luke Palmer <lctpalmer@gmail.com>
2014-10-27 17:54:05 +0000

    Updated comments

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ emcee<br>
 csv<br>
 <br>
 Includes:<br>
-mcmc_tc.py - routines for estimating Von Mises model parameters.<br>
+mcmc_tc.py - routines for estimating Von Mises model parameters<br>
 mcmc_plot.py - plotting the results of previous analysis<br>
 demo.py - demonstration of fitting a Von Mises function to a single voxel using real fMRI data. Also computes the Bayes factor of the Von Mises model over an untuned model<br>
-example_data.csv - data from an orientation tuning experiment. Columns 3 to end are detrended and z-normalised BOLD timeseries for each voxel in V1 of this subject (V1 voxels were isolated using retinotopic mapping in a separate analysis)
+example_data.csv - data from an orientation tuning experiment. Columns 3 to end are detrended and z-normalised BOLD timeseries for each voxel in V1 (V1 voxels were isolated using retinotopic mapping in a separate analysis)
 
 
--- demo.py
+++ demo.py
@@ -17,22 +17,21 @@ data = np.array(data)
 data = data.astype(np.float)
 voxel_num = 497
 y = data[:,voxel_num+3]
-x = 2*(np.pi/180)*((data[:,3]-1)*20)#spread over 2pi
+x = 2*(np.pi/180)*((data[:,3]-1)*20)#multiply condition number by 20 degrees (and scale to 360 for the vm)
 
 #run mcmc
 sampler_vm = mcmc_tc.mcmc_est(y,x,mcmc_tc.vm_func,mcmc_tc.log_like_addgauss, ndim=5)
 sampler_flat = mcmc_tc.mcmc_est(y,x,mcmc_tc.flat_func,mcmc_tc.log_like_addgauss, ndim=2)
-burnin = 1000#first 1000 samples 'burning in'
 
 #model comparison (calc evidence for tuned model, bf > 10 is good evidence)
 bf = mcmc_tc.bayes_factor(sampler_vm, sampler_flat)
 print(bf)
 
 #calc fwhm of vm model
-vmf = 0.5*mcmc_tc.vm_fwhm(sampler_vm.flatchain[:,3])#0.5 bevause vm is on 2pi
+vmf = 0.5*mcmc_tc.vm_fwhm(sampler_vm.flatchain[:,3])#0.5 bevause vm is across 2pi
 vm_max, vm_min = mcmc_tc.vm_maxmin(sampler_vm.flatchain)
-vmb = vm_min#'real' baseline
-vma = vm_max-vm_min#'real' amplitude
+vmb = vm_min#baseline BOLD
+vma = vm_max-vm_min#amplitude
 
 ##plot results
 burnin = 1000#first 1000 samples 'burning in'
